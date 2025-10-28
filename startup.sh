10371ccf70b108eb5b0f9959b897d46fb3ad1fe7
Hani Abraksiea <hani7n@yahoo.com>
2025-10-28 14:07:43 +0100

    Fix Azure startup script

--- startup.sh
+++ startup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-echo "🔹 Starting Streamlit app inside Azure..."
+echo "🔹 Custom startup for Azure - installing dependencies and running Streamlit..."
 cd /home/site/wwwroot
 
-# Installera beroenden (lokalt i användarens pip-cache)
+# Installera beroenden i användarens katalog (Azure tillåter bara skrivning där)
 pip install --no-cache-dir --user -r requirements.txt
 
-# Kör appen
+# Kör Streamlit från rätt plats
 ~/.local/bin/streamlit run app.py --server.port 8000 --server.address 0.0.0.0
