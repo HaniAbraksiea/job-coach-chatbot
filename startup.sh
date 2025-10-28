ad654d50d134177563dbad4d70f087a3f1a7475d
Hani Abraksiea <hani7n@yahoo.com>
2025-10-28 12:38:18 +0100

    Fix: ändrade radslut till LF och gjorde startup.sh körbar

--- startup.sh
+++ startup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+cd /home/site/wwwroot
+echo "Starting Streamlit app..."
+# Installera beroenden (för säkerhets skull)
+pip install -r requirements.txt
+# Starta appen på port 8000 (Azure kräver denna)
 streamlit run app.py --server.port 8000 --server.address 0.0.0.0
-
-# chmod +x startup.sh
